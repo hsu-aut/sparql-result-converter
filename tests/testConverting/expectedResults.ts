e52a21615b86f53da57bf901eb1a974869cfbd5b
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-17 09:36:20 +0200

    Set to indendation to tabs

--- tests/testConverting/expectedResults.ts
+++ tests/testConverting/expectedResults.ts
@@ -1,86 +1,86 @@
 // Expected result for one layer mapping:
 export const expectedOneLayerResult = [
-    {
-        ownerName: 'Peter',
-        pets: [
-            {
-                petName: 'Rex',
-                petType: 'Dog'
-            }
-        ]
-    },
-    {
-        ownerName: 'John',
-        pets: [
-            {
-                petName: 'Lassie',
-                petType: 'Dog'
-            },
-            {
-                petName: 'Oliver',
-                petType: 'Cat'
-            }
-        ]
-    },
-    {
-        ownerName: 'Mary',
-        pets: [
-            {
-                petName: 'Huey',
-                petType: 'Cat'
-            },
-            {
-                petName: 'Dewey',
-                petType: 'Cat'
-            },
-            {
-                petName: 'Louie',
-                petType: 'Cat'
-            }
-        ]
-    }
+	{
+		ownerName: 'Peter',
+		pets: [
+			{
+				petName: 'Rex',
+				petType: 'Dog'
+			}
+		]
+	},
+	{
+		ownerName: 'John',
+		pets: [
+			{
+				petName: 'Lassie',
+				petType: 'Dog'
+			},
+			{
+				petName: 'Oliver',
+				petType: 'Cat'
+			}
+		]
+	},
+	{
+		ownerName: 'Mary',
+		pets: [
+			{
+				petName: 'Huey',
+				petType: 'Cat'
+			},
+			{
+				petName: 'Dewey',
+				petType: 'Cat'
+			},
+			{
+				petName: 'Louie',
+				petType: 'Cat'
+			}
+		]
+	}
 ];
 
 export const expectedTwoLayerResult = [
-    {
-        ownerName: 'Peter',
-        petTypes: [
-            {
-                type: 'Dog',
-                pets: [
-                    { petName: 'Rex' }
-                ]
-            }
-        ]
-    },
-    {
-        ownerName: 'John',
-        petTypes: [
-            {
-                type: 'Dog',
-                pets: [
-                    { petName: 'Lassie' }
-                ]
-            },
-            {
-                type: 'Cat',
-                pets: [
-                    { petName: 'Oliver' }
-                ]
-            }
-        ]
-    },
-    {
-        ownerName: 'Mary',
-        petTypes: [
-            {
-                type: 'Cat',
-                pets: [
-                    { petName: 'Huey' },
-                    { petName: 'Dewey' },
-                    { petName: 'Louie' },
-                ]
-            }
-        ]
-    }
+	{
+		ownerName: 'Peter',
+		petTypes: [
+			{
+				type: 'Dog',
+				pets: [
+					{ petName: 'Rex' }
+				]
+			}
+		]
+	},
+	{
+		ownerName: 'John',
+		petTypes: [
+			{
+				type: 'Dog',
+				pets: [
+					{ petName: 'Lassie' }
+				]
+			},
+			{
+				type: 'Cat',
+				pets: [
+					{ petName: 'Oliver' }
+				]
+			}
+		]
+	},
+	{
+		ownerName: 'Mary',
+		petTypes: [
+			{
+				type: 'Cat',
+				pets: [
+					{ petName: 'Huey' },
+					{ petName: 'Dewey' },
+					{ petName: 'Louie' },
+				]
+			}
+		]
+	}
 ];
--- tests/testConverting/testConverting.test.ts
+++ tests/testConverting/testConverting.test.ts
@@ -9,38 +9,39 @@ const resultConverter = new SparqlResultConverter();
 
 
 describe('One Layer Test', () => {
-    it('Should group a result on one layer', () => {
-    // Object that defines the structure of the result
-        const oneLayerMappingDefinition = [
-            {
-                objectToGroup: 'owner',
-                name: 'ownerName',
-                childRoot: 'pets'
-            }
-        ];
+	it('Should group a result on one layer', () => {
+		// Object that defines the structure of the result
+		const oneLayerMappingDefinition = [[
+			{
+				objectToGroup: 'owner',
+				name: 'ownerName',
+				childRoot: 'pets'
+			}
+		]
+		];
 
-        const convertedResult = resultConverter.convert(testData.results, oneLayerMappingDefinition);
-        assert.deepEqual(convertedResult, expectedOneLayerResult, 'Testing one layer conversion failed...');
-    });
+		const convertedResult = resultConverter.convert(testData.results, oneLayerMappingDefinition);
+		assert.deepEqual(convertedResult, expectedOneLayerResult, 'Testing one layer conversion failed...');
+	});
 });
 
 describe('Two Layer Test', () => {
-    it('Should group a result on two layers', () => {
-    // Object that defines the structure of the result
-        const twoLayerMappingDefinition = [
-            {
-                objectToGroup: 'owner',
-                name: 'ownerName',
-                childRoot: 'petTypes'
-            },
-            {
-                objectToGroup: 'petType',
-                name: 'type',
-                childRoot: 'pets'
-            }
-        ];
-        const convertedResult = resultConverter.convert(testData.results, twoLayerMappingDefinition);
+	it('Should group a result on two layers', () => {
+		// Object that defines the structure of the result
+		const twoLayerMappingDefinition = [[
+			{
+				objectToGroup: 'owner',
+				name: 'ownerName',
+				childRoot: 'petTypes'
+			},
+			{
+				objectToGroup: 'petType',
+				name: 'type',
+				childRoot: 'pets'
+			}
+		]];
+		const convertedResult = resultConverter.convert(testData.results, twoLayerMappingDefinition);
 
-        assert.deepEqual(convertedResult, expectedTwoLayerResult, 'Testing two layer conversion failed...');
-    });
+		assert.deepEqual(convertedResult, expectedTwoLayerResult, 'Testing two layer conversion failed...');
+	});
 });
