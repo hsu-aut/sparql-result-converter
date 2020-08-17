b981577875a0f4cbc99a9f1551840d848415d8ba
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-17 09:36:20 +0200

    Set to indendation to tabs

--- src/SparqlResultConverter.ts
+++ src/SparqlResultConverter.ts
@@ -4,89 +4,90 @@ import { ArrayUtil, SparqlResultLine } from "./ArrayUtil";
 
 // Maps the query result of "select_allModules" to an array of Modules
 export class SparqlResultConverter {
-    /**
- * Groups a table-structure and converts it to a tree-like structure
- * @param {*} inputArray An array representing data structured as a table
- * @param {*} mappingDefinitions An array of objects representing the structure of the final output
- */
-    convert(inputArray: SparqlResultLine[], mappingDefinitions: MappingDefinition[], currElement = 0): unknown[] {
-        let flattenedArray;
 
-        // first: transform array
-        if (currElement === 0) {
-            flattenedArray = ArrayUtil.extractValues(inputArray);
-        } else {
-            flattenedArray = inputArray;
-        }
+	/**
+    * Groups a table-structure and converts it to a tree-like structure
+ 	* @param {*} inputArray An array representing data structured as a table
+ 	* @param {*} mappingDefinitions An array of objects representing the structure of the final output
+ 	*/
+	convert(inputArray: SparqlResultLine[], mappingDefinitions: MappingDefinition[], currElement = 0): unknown[] {
+		let flattenedArray;
 
-        // get the current mapping object
-        const currentMappingDefition = mappingDefinitions[currElement];
+		// first: transform array
+		if (currElement === 0) {
+			flattenedArray = ArrayUtil.extractValues(inputArray);
+		} else {
+			flattenedArray = inputArray;
+		}
 
-        // group the flattened array by the current mapping object's "objectToGroup" property
-        const groupedObject = groupBy(flattenedArray, (elem) => elem[currentMappingDefition.objectToGroup]);
+		// get the current mapping object
+		const currentMappingDefition = mappingDefinitions[currElement];
 
-        // Empty the outputArray array, it will later be filled with the grouped content
-        const outputArray = [];
+		// group the flattened array by the current mapping object's "objectToGroup" property
+		const groupedObject = groupBy(flattenedArray, (elem) => elem[currentMappingDefition.objectToGroup]);
 
-        Object.keys(groupedObject).forEach((key) => {
-            let groupedElement = groupedObject[key];
+		// Empty the outputArray array, it will later be filled with the grouped content
+		const outputArray = [];
 
-            // Collect all elements that should be collected
-            // TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
-            const elemsToCollect = {};
-            if(currentMappingDefition.toCollect) {
-                currentMappingDefition.toCollect.forEach((elemToCollect) => {
-                    elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
-                    groupedElement.forEach((inputElem) => {
-                        delete inputElem[elemToCollect];
-                    });
-                });
-            }
+		Object.keys(groupedObject).forEach((key) => {
+			let groupedElement = groupedObject[key];
 
-
-            if (currElement <= (mappingDefinitions.length - 2)) {
-                if (ArrayUtil.allEntriesContainGroupingProperty(groupedElement, mappingDefinitions[currElement + 1].objectToGroup)) {
-                    groupedElement = (this.convert(groupedElement, mappingDefinitions, currElement + 1));
-                }
-            }
+			// Collect all elements that should be collected
+			// TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
+			const elemsToCollect = {};
+			if(currentMappingDefition.toCollect) {
+				currentMappingDefition.toCollect.forEach((elemToCollect) => {
+					elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
+					groupedElement.forEach((inputElem) => {
+						delete inputElem[elemToCollect];
+					});
+				});
+			}
 
 
-            // Delete the all elements that have already been grouped
-            groupedElement.forEach((element) => {
-                mappingDefinitions.forEach((mapDef) => {
-                    delete element[mapDef.objectToGroup];
-                });
-            });
+			if (currElement <= (mappingDefinitions.length - 2)) {
+				if (ArrayUtil.allEntriesContainGroupingProperty(groupedElement, mappingDefinitions[currElement + 1].objectToGroup)) {
+					groupedElement = (this.convert(groupedElement, mappingDefinitions, currElement + 1));
+				}
+			}
 
 
-            const nameToPush = {
-                [currentMappingDefition.name]: key,
-            };
-
-            let objToPush = {};
-            if (!isEmpty(groupedElement[0])) {
-                const groupToPush = {
-                    [currentMappingDefition.childRoot]: groupedElement
-                };
-                objToPush = {
-                    ...nameToPush,
-                    ...elemsToCollect,
-                    ...groupToPush
-                };
-            } else {
-                objToPush = {
-                    ...nameToPush,
-                    ...elemsToCollect
-                };
-            }
+			// Delete the all elements that have already been grouped
+			groupedElement.forEach((element) => {
+				mappingDefinitions.forEach((mapDef) => {
+					delete element[mapDef.objectToGroup];
+				});
+			});
 
 
-            // Add the grouped element to the outputArray
-            outputArray.push(objToPush);
-        });
+			const nameToPush = {
+				[currentMappingDefition.name]: key,
+			};
 
-        return outputArray;
-    }
+			let objToPush = {};
+			if (!isEmpty(groupedElement[0])) {
+				const groupToPush = {
+					[currentMappingDefition.childRoot]: groupedElement
+				};
+				objToPush = {
+					...nameToPush,
+					...elemsToCollect,
+					...groupToPush
+				};
+			} else {
+				objToPush = {
+					...nameToPush,
+					...elemsToCollect
+				};
+			}
+
+
+			// Add the grouped element to the outputArray
+			outputArray.push(objToPush);
+		});
+
+		return outputArray;
+	}
 }
 
 
--- tests/testArrayUtil/ArrayUtil.test.ts
+++ tests/testArrayUtil/ArrayUtil.test.ts
@@ -3,92 +3,92 @@ import { ArrayUtil, SparqlResultLine } from '../../src/ArrayUtil';
 
 
 describe('ArrayUtil Test', () => {
-    it('Should extract value property of each entry', () => {
-    // Object that defines the structure of the result
-        const input: SparqlResultLine[] =
-      [
-          {
-              "capability": {
-                  "type": "uri",
-                  "value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability"
-              },
-              "skill": {
-                  "type": "uri",
-                  "value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill"
-              }
-          },
-          {
-              "capability": {
-                  "type": "uri",
-                  "value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability"
-              },
-              "skill": {
-                  "type": "uri",
-                  "value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill_REST"
-              }
-          }
-      ];
+	it('Should extract value property of each entry', () => {
+		// Object that defines the structure of the result
+		const input: SparqlResultLine[] =
+		[
+			{
+				"capability": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill"
+				}
+			},
+			{
+				"capability": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill_REST"
+				}
+			}
+		];
 
-        const expectedResult = [
-            {
-                capability: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability",
-                skill: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill"
-            },
-            {
-                capability: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability",
-                skill: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill_REST"
-            }
-        ];
-
-
-        const transformedArray = ArrayUtil.extractValues(input);
-        assert.deepEqual(transformedArray, expectedResult, 'Expected result should have all values extracted...');
-    });
+		const expectedResult = [
+			{
+				capability: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability",
+				skill: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill"
+			},
+			{
+				capability: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenCapability",
+				skill: "http://www.hsu-ifa.de/ontologies/capability-example#FraesenSkill_REST"
+			}
+		];
 
 
-    it('Should confirm that all array entries contain grouping property', () => {
-    // Just some random array
-        const input =
-      [
-          {
-              name: "Peter",
-              age: 55
-          },
-          {
-              name: "John",
-              age: 43
-          },
-          {
-              name: "Mary",
-              age: 29
-          }
-      ];
-
-        const result = ArrayUtil.allEntriesContainGroupingProperty(input, "name");
-        assert.isTrue(result, 'Should return true because all array entries contain grouping property...');
-    });
+		const transformedArray = ArrayUtil.extractValues(input);
+		assert.deepEqual(transformedArray, expectedResult, 'Expected result should have all values extracted...');
+	});
 
 
-    it('Should confirm that not all array entries contain grouping property', () => {
-    // Just some random array
-        const input =
-      [
-          {
-              name: "Peter",
-              age: 55,
-              city: "Boston"
-          },
-          {
-              name: "John",
-              age: 43,
-              city: "New York City"
-          },
-          {
-              name: "Mary",
-              age: 29
-          }
-      ];
-        const result = ArrayUtil.allEntriesContainGroupingProperty(input, "city");
-        assert.isFalse(result, 'Should return false because not all array entries contain grouping property...');
-    });
+	it('Should confirm that all array entries contain grouping property', () => {
+		// Just some random array
+		const input =
+		[
+			{
+				name: "Peter",
+				age: 55
+			},
+			{
+				name: "John",
+				age: 43
+			},
+			{
+				name: "Mary",
+				age: 29
+			}
+		];
+
+		const result = ArrayUtil.allEntriesContainGroupingProperty(input, "name");
+		assert.isTrue(result, 'Should return true because all array entries contain grouping property...');
+	});
+
+
+	it('Should confirm that not all array entries contain grouping property', () => {
+		// Just some random array
+		const input =
+		[
+			{
+				name: "Peter",
+				age: 55,
+				city: "Boston"
+			},
+			{
+				name: "John",
+				age: 43,
+				city: "New York City"
+			},
+			{
+				name: "Mary",
+				age: 29
+			}
+		];
+		const result = ArrayUtil.allEntriesContainGroupingProperty(input, "city");
+		assert.isFalse(result, 'Should return false because not all array entries contain grouping property...');
+	});
 });
