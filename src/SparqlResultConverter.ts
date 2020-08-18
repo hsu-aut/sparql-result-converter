18abbad96fe86366d92389cf858a5b903539a795
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-18 14:39:18 +0200

    first steps to allowing nested mappings

--- src/SparqlResultConverter.ts
+++ src/SparqlResultConverter.ts
@@ -5,88 +5,106 @@ import { ArrayUtil, SparqlResultLine } from "./ArrayUtil";
 // Maps the query result of "select_allModules" to an array of Modules
 export class SparqlResultConverter {
 
+	convert(inputArray: SparqlResultLine[], mappingDefinitions: MappingDefinition[]): any {
+		const flattenedArray = ArrayUtil.extractValues(inputArray);
+		return this.convert2(flattenedArray, mappingDefinitions);
+	}
+
 	/**
     * Groups a table-structure and converts it to a tree-like structure
  	* @param {*} inputArray An array representing data structured as a table
  	* @param {*} mappingDefinitions An array of objects representing the structure of the final output
  	*/
-	convert(inputArray: SparqlResultLine[], mappingDefinitions: MappingDefinition[], currElement = 0): unknown[] {
-		let flattenedArray;
+	convert2(inputArray: Record<string, string>[], mappingDefinitions: Partial<MappingDefinition>[]): Record<string,Array<any>> {
+		const outputObject = {};
 
-		// first: transform array
-		if (currElement === 0) {
-			flattenedArray = ArrayUtil.extractValues(inputArray);
-		} else {
-			flattenedArray = inputArray;
-		}
+		mappingDefinitions.forEach(mappingDefinition => {
+			outputObject[mappingDefinition.rootName] = new Array<any>();
 
-		// get the current mapping object
-		const currentMappingDefition = mappingDefinitions[currElement];
+			// group the flattened array by the current mapping object's "objectToGroup" property
+			if(!inputArray.some(elem => elem[mappingDefinition.objectToGroup])) return null;
+			const groupedObject = groupBy(inputArray, (elem) => elem[mappingDefinition.objectToGroup]) as Record<string, any>;
 
-		// group the flattened array by the current mapping object's "objectToGroup" property
-		const groupedObject = groupBy(flattenedArray, (elem) => elem[currentMappingDefition.objectToGroup]);
+			// delete ungrouped (undefined) values
+			delete groupedObject["undefined"];
 
-		// Empty the outputArray array, it will later be filled with the grouped content
-		const outputArray = [];
 
-		Object.keys(groupedObject).forEach((key) => {
-			let groupedElement = groupedObject[key];
+			for (const key in groupedObject) {
+				let groupedElement = groupedObject[key];
 
-			// Collect all elements that should be collected
-			// TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
-			const elemsToCollect = {};
-			if(currentMappingDefition.toCollect) {
-				currentMappingDefition.toCollect.forEach((elemToCollect) => {
-					elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
-					groupedElement.forEach((inputElem) => {
-						delete inputElem[elemToCollect];
+				if (groupedElement.length === 0) break;
+
+				// Collect all elements that should be collected
+				// TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
+				const elemsToCollect = {};
+				if(mappingDefinition.toCollect) {
+					mappingDefinition.toCollect.forEach((elemToCollect) => {
+						elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
+						groupedElement.forEach((inputElem) => {
+							delete inputElem[elemToCollect];
+						});
 					});
-				});
-			}
-
-
-			if (currElement <= (mappingDefinitions.length - 2)) {
-				if (ArrayUtil.allEntriesContainGroupingProperty(groupedElement, mappingDefinitions[currElement + 1].objectToGroup)) {
-					groupedElement = (this.convert(groupedElement, mappingDefinitions, currElement + 1));
 				}
-			}
-
-
-			// Delete the all elements that have already been grouped
-			groupedElement.forEach((element) => {
-				mappingDefinitions.forEach((mapDef) => {
-					delete element[mapDef.objectToGroup];
+				// Delete the all elements that have already been grouped
+				groupedElement.forEach((element) => {
+					delete element[mappingDefinition.objectToGroup];
 				});
-			});
 
 
-			const nameToPush = {
-				[currentMappingDefition.name]: key,
-			};
+				let objToPush = {};
+				const nameToPush = {
+					[mappingDefinition.name]: key,
+				};
 
-			let objToPush = {};
-			if (!isEmpty(groupedElement[0])) {
-				const groupToPush = {
-					[currentMappingDefition.childRoot]: groupedElement
-				};
-				objToPush = {
-					...nameToPush,
-					...elemsToCollect,
-					...groupToPush
-				};
-			} else {
-				objToPush = {
-					...nameToPush,
-					...elemsToCollect
-				};
+
+				if (mappingDefinition.childMappings && mappingDefinition.childMappings[0].objectToGroup){
+				// if (ArrayUtil.allEntriesContainGroupingProperty(groupedElement, mappingDefinition.childMappings .objectToGroup)) {
+					groupedElement = this.convert2(groupedElement, mappingDefinition.childMappings);
+					// }
+
+
+
+					// if (!isEmpty(groupedElement[0])) {
+
+					// const groupToPush = {
+					// 	[mappingDefinition.childMappings[0].rootName] : groupedElement
+					// };
+					objToPush = {
+						...nameToPush,
+						...elemsToCollect,
+						...groupedElement
+					};
+
+				} else {
+					let rootName;
+					(mappingDefinition.childMappings) ? rootName = mappingDefinition.childMappings[0].rootName : rootName ="children";
+
+					const groupToPush = {
+						[rootName] : groupedElement
+					};
+					objToPush = {
+						...nameToPush,
+						...elemsToCollect,
+						...groupToPush
+					};
+				}
+
+
+				// } else {
+				// objToPush = {
+				// 	...nameToPush,
+				// 	...elemsToCollect
+				// };
+
+				// }
+
+				outputObject[mappingDefinition.rootName].push(objToPush);
+
+
 			}
 
-
-			// Add the grouped element to the outputArray
-			outputArray.push(objToPush);
 		});
-
-		return outputArray;
+		return outputObject;
 	}
 }
 
@@ -96,5 +114,5 @@ export interface MappingDefinition {
 	objectToGroup: string,
 	name: string,
 	toCollect?: string[];
-	childMappings?: MappingDefinition[],
+	childMappings?: Partial<MappingDefinition>[],
 }
