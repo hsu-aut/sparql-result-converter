2966e350849f92f6cbfc41b44dae215a2daf800c
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-18 16:34:50 +0200

    Adapted tests and data to new structure

--- tests/testMultipleOnEachLayer/testConverting.test.ts
+++ tests/testMultipleOnEachLayer/testConverting.test.ts
@@ -1,35 +1,64 @@
 import { assert } from 'chai';
-import { expectedResult as expectedResult } from './expectedResults';
+import { expectedTwoLayerResult, expectedOneLayerResult } from './expectedResults';
 
-import { SparqlResultConverter } from "../../src/SparqlResultConverter";
+import { SparqlResultConverter, MappingDefinition } from "../../src/SparqlResultConverter";
 
-import {testData} from './test-data';
+import {oneLayerTestData, twoLayerTestData} from './test-data';
 
 const resultConverter = new SparqlResultConverter();
 
 
-describe('One Layer Test with multiple properties on one layer', () => {
+describe('One-Layer-Test with multiple properties on this layer', () => {
 	it('Should group a result on one layer respecting multiple properties', () => {
 		// Object that defines the structure of the result
-		const oneLayerMappingDefinition = [[
+		const oneLayerMappingDefinition: MappingDefinition[] = [
 			{
-				objectToGroup: 'owner',
-				name: 'ownerName',
-				childRoot: 'dogs'
+				rootName: 'men',
+				propertyToGroup: 'maleOwner',
+				name: 'name',
+				childMappings: [{
+					rootName: 'pets',
+				}],
 			},
 			{
-				objectToGroup: 'dogName',
-				name: 'petName',
-				childRoot: 'cats'
-			},
-			{
-				objectToGroup: 'catName',
-				name: 'petName',
-				childRoot: 'dogs'
-			}
-		]];
+				rootName: 'women',
+				propertyToGroup: 'femaleOwner',
+				name: 'name',
+				childMappings: [{
+					rootName: 'pets',
+				}],
+			}];
 
-		const convertedResult = resultConverter.convert(testData.results, oneLayerMappingDefinition);
-		assert.deepEqual(convertedResult, expectedResult, 'Testing one layer conversion failed...');
+		const convertedResult = resultConverter.convertToDefinition(oneLayerTestData.results, oneLayerMappingDefinition);
+		assert.deepEqual(convertedResult, expectedOneLayerResult, 'Testing one layer conversion failed...');
+	});
+});
+
+
+
+describe('Two-Layer-Test with multiple properties on one layer', () => {
+	it('Should group a result on two layers respecting multiple properties', () => {
+		// Object that defines the structure of the result
+		const twoLayerMappingDefinition: MappingDefinition[] = [
+			{
+				rootName: 'owners',
+				propertyToGroup: 'owner',
+				name: 'ownerName',
+				childMappings: [{
+					rootName: 'dogs',
+					propertyToGroup: 'dogName',
+					name: 'petName',
+				},
+				{
+					rootName: 'cats',
+					propertyToGroup: 'catName',
+					name: 'petName',
+				}
+				]
+			},
+		];
+
+		const convertedResult = resultConverter.convertToDefinition(twoLayerTestData.results, twoLayerMappingDefinition);
+		assert.deepEqual(convertedResult, expectedTwoLayerResult, 'Testing one layer conversion failed...');
 	});
 });
