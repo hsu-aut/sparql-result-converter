3738d5d94586b42295de282ea2abddcfcdf4b913
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-08-21 11:18:46 +0200

    Fixed a bug that caused toCollect not to work

--- src/SparqlResultConverter.ts
+++ src/SparqlResultConverter.ts
@@ -1,5 +1,5 @@
 import { groupBy } from "lodash";
-import isEmpty = require ("lodash");
+import isEmpty = require ("lodash.isempty");
 import { ArrayUtil, SparqlResultLine } from "./ArrayUtil";
 
 // Maps the query result of "select_allModules" to an array of Modules
@@ -21,75 +21,120 @@ export class SparqlResultConverter {
 		mappingDefinitions.forEach(mappingDefinition => {
 
 			// if no element of the input array has the property to group on, there's nothing to do return
-			if(!inputArray.some(elem => elem[mappingDefinition.propertyToGroup])) return null;
+			// if(!inputArray.some(elem => elem[mappingDefinition.propertyToGroup])) return null;
+
 
 			// create a new array with the key [rootName], this will hold the grouped array
-			outputObject[mappingDefinition.rootName] = new Array<unknown>();
 
-			const groupedObject = groupBy(inputArray, (elem) => elem[mappingDefinition.propertyToGroup]) as Record<string, Array<unknown>>;
 
-			// delete ungrouped (undefined) values
-			delete groupedObject["undefined"];
+			let groupedObject;
+			if (mappingDefinition.propertyToGroup && inputArray.some(elem => elem[mappingDefinition.propertyToGroup])) {
+				outputObject[mappingDefinition.rootName] = new Array<unknown>();
+				groupedObject = this.groupArray(inputArray, mappingDefinition);
 
-			for (const key in groupedObject) {
-				const groupedElement = groupedObject[key];
+				for (const key in groupedObject) {
+					const groupedElement = groupedObject[key];
 
-				if (groupedElement.length === 0) break;
 
-				// Collect all elements that should be collected
-				// TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
-				const elemsToCollect = {};
-				if(mappingDefinition.toCollect) {
-					mappingDefinition.toCollect.forEach((elemToCollect) => {
-						elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
-						groupedElement.forEach((inputElem) => {
-							delete inputElem[elemToCollect];
+					// Collect all elements that should be collected
+					// TODO: Not only take groupedElement[0], but make sure the properties to collect are equal for all groupedElements
+					const elemsToCollect = {};
+					if(mappingDefinition.toCollect) {
+						mappingDefinition.toCollect.forEach((elemToCollect) => {
+							elemsToCollect[elemToCollect] = groupedElement[0][elemToCollect];
+							groupedElement.forEach((elem) => {
+								delete elem[elemToCollect];
+							});
 						});
-					});
-				}
-
-				// The common element (propertyToGroup) has been extracted, delete it from sub elements
-				groupedElement.forEach((element) => {
-					delete element[mappingDefinition.propertyToGroup];
-				});
+					}
 
 
-				let objToPush = {};
-				const nameToPush = {
-					[mappingDefinition.name]: key,
-				};
-
-				let groupedArrayToPush : Record<string, Array<unknown>>;
-				let rootName;
-				if (mappingDefinition.childMappings && mappingDefinition.childMappings[0].propertyToGroup){
-					// If there are "real" childMappings (with a propertyToGroup) -> call convert() recursively on the groupedElement
-					groupedArrayToPush = this.convert(groupedElement as Record<string, string>[], mappingDefinition.childMappings);
-				} else {
-					// If there are no more propertiesToGroup: put this element under the subElements rootName (if not set, take "children" as default)
-					(mappingDefinition.childMappings) ? rootName = mappingDefinition.childMappings[0].rootName : rootName ="children";
-					groupedArrayToPush = {[rootName]: groupedElement};
-				}
-
-				// if the default rootName has been used and the element is empty -> don't add groupedArrayToPush to the object to add
-				if(rootName == "children" && isEmpty(groupedElement[0])) {
-					objToPush = {
-						...nameToPush,
-						...elemsToCollect,
+					let objToPush = {};
+					const nameToPush = {
+						[mappingDefinition.name]: key,
 					};
-				} else {
+
+					let groupedArrayToPush : Record<string, Array<unknown>>;
+					let rootName;
+					if (mappingDefinition.childMappings){
+						// If there are "real" childMappings (with a propertyToGroup) -> call convert() recursively on the groupedElement
+						groupedArrayToPush = this.convert(groupedElement as Record<string, string>[], mappingDefinition.childMappings);
+					}
+					// else {
+					// 	// If there are no more propertiesToGroup: put this element under the subElements rootName (if not set, take "children" as default)
+					// 	(mappingDefinition.childMappings) ? rootName = mappingDefinition.childMappings[0].rootName : rootName ="children";
+					// 	groupedArrayToPush = {[rootName]: groupedElement};
+					// }
+
+					// if the default rootName has been used and the element is empty -> don't add groupedArrayToPush to the object to add
+					// if(rootName == "children" && isEmpty(groupedElement[0])) {
+					// 	objToPush = {
+					// 		...nameToPush,
+					// 		...elemsToCollect,
+					// 	};
+					// } else {
 					objToPush = {
 						...nameToPush,
 						...elemsToCollect,
 						...groupedArrayToPush
 					};
-				}
+					// }
 
-				outputObject[mappingDefinition.rootName].push(objToPush);
+					outputObject[mappingDefinition.rootName].push(objToPush);
+
+				}
+			} else {
+				// let rootName: string;
+				// If there are no more propertiesToGroup: put this element under the subElements rootName (if not set, take "children" as default)
+				// (mappingDefinition.childMappings == undefined) ? rootName = mappingDefinition.rootName : rootName ="children";
+				const groupedArrayToPush = inputArray;
+				// const elemsToCollect = [];
+				// if(mappingDefinition.toCollect) {
+				// 	mappingDefinition.toCollect.forEach((elemToCollect) => {
+				// 		elemsToCollect[elemToCollect] = inputArray[0][elemToCollect];
+				// 		// groupedArrayToPush.forEach((inputElem) => {
+				// 		// 	delete inputElem[elemToCollect];
+				// 		// });
+				// 	});
+				// }
+				// let objToPush;
+				if(!inputArray.every(elem => isEmpty(elem))) {
+					outputObject[mappingDefinition.rootName] = new Array<unknown>();
+
+					// if(!isEmpty(inputArray)) {
+					// 	objToPush = {
+					// 		...elemsToCollect,
+					// 	};
+					// } else {
+					const objToPush = {
+						// ...elemsToCollect,
+						...groupedArrayToPush
+					};
+					outputObject[mappingDefinition.rootName].push(...groupedArrayToPush);
+				}
 
 			}
 
 		});
 		return outputObject;
+	}
+
+
+	private groupArray(inputArray: Record<string, string>[], mappingDefinition: Partial<MappingDefinition>): Record<string, Array<unknown>> {
+
+		const groupedObject = groupBy(inputArray, (elem) => elem[mappingDefinition.propertyToGroup]) as Record<string, Array<unknown>>;
+		// delete ungrouped (undefined) values
+		delete groupedObject["undefined"];
+
+		// The common element (propertyToGroup) has been extracted, delete it from sub elements
+		for (const key in groupedObject) {
+			const groupedEntry = groupedObject[key];
+			groupedEntry.forEach((element) => {
+				delete element[mappingDefinition.propertyToGroup];
+			});
+		}
+
+		return groupedObject;
 	}
 }
 
@@ -99,5 +144,6 @@ export interface MappingDefinition {
 	propertyToGroup: string,
 	name: string,
 	toCollect?: string[];
-	childMappings?: Partial<MappingDefinition>[],
+	childMappings?: Array<Partial<MappingDefinition> & Pick<Required<MappingDefinition>, "rootName">,
 }
+
