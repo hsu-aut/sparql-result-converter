b4dad3e4c8b58f80c228a38ffba65162add17b4e
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-09-06 13:32:20 +0200

    Added a more complex test

---
+++ tests/skillTests/expectedResults.ts
@@ -0,0 +1,35 @@
+// Expected result for a very simple one layer mapping including non-grouped children elements:
+export const skillResult = {
+	skills : [
+		{
+			skillIri: 'https://siemens.de/skills#OpcUaSkill',
+			stateMachine: 'https://siemens.de/skills#OpcUaSkill_StateMachine',
+			currentStateTypeIri: 'http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle',
+			skillParameters: [
+				{
+					parameterIri: 'https://siemens.de/skills#OpcUaSkill_a',
+					parameterName: 'a',
+					parameterType: 'Integer',
+					parameterRequired: 'true',
+					parameterDefault: '0',
+					parameterOptionValues: [ { value: '2' }, { value: '3' }, { value: '4' } ]
+				},
+				{
+					parameterIri: 'https://siemens.de/skills#OpcUaSkill_b',
+					parameterName: 'b',
+					parameterType: 'Integer',
+					parameterRequired: 'true',
+					parameterDefault: '0',
+				}
+			],
+			skillOutputs: [
+				{
+					outputIri: 'https://siemens.de/skills#OpcUaSkill_result',
+					outputName: 'result',
+					outputType: 'Integer',
+					outputRequired: 'false'
+				}
+			]
+		}
+	]
+};
---
+++ tests/skillTests/test-data.ts
@@ -0,0 +1,240 @@
+/**
+ * Mock-up of a piece of data that could be returned from a DB-Query.
+ */
+export const testData = {
+	"head": {
+		"vars": [
+			"skill",
+			"stateMachine",
+			"currentStateTypeIri",
+			"parameterIri",
+			"parameterName",
+			"parameterType",
+			"parameterRequired",
+			"parameterDefault",
+			"paramOptionValue",
+			"outputIri",
+			"outputName",
+			"outputType",
+			"outputRequired",
+			"outputDefault",
+			"outputOptionValue"
+		]
+	},
+	"results": {
+		"bindings": [
+			{
+				"parameterType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"outputName": {
+					"type": "literal",
+					"value": "result"
+				},
+				"currentStateTypeIri": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle"
+				},
+				"outputType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"paramOptionValue": {
+					"type": "literal",
+					"value": "4"
+				},
+				"parameterName": {
+					"type": "literal",
+					"value": "a"
+				},
+				"parameterDefault": {
+					"type": "literal",
+					"value": "0"
+				},
+				"outputIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_result"
+				},
+				"stateMachine": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_StateMachine"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill"
+				},
+				"outputRequired": {
+					"type": "literal",
+					"value": "false"
+				},
+				"parameterIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_a"
+				},
+				"parameterRequired": {
+					"type": "literal",
+					"value": "true"
+				}
+			},
+			{
+				"parameterType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"outputName": {
+					"type": "literal",
+					"value": "result"
+				},
+				"currentStateTypeIri": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle"
+				},
+				"outputType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"paramOptionValue": {
+					"type": "literal",
+					"value": "3"
+				},
+				"parameterName": {
+					"type": "literal",
+					"value": "a"
+				},
+				"parameterDefault": {
+					"type": "literal",
+					"value": "0"
+				},
+				"outputIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_result"
+				},
+				"stateMachine": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_StateMachine"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill"
+				},
+				"outputRequired": {
+					"type": "literal",
+					"value": "false"
+				},
+				"parameterIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_a"
+				},
+				"parameterRequired": {
+					"type": "literal",
+					"value": "true"
+				}
+			},
+			{
+				"parameterType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"outputName": {
+					"type": "literal",
+					"value": "result"
+				},
+				"currentStateTypeIri": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle"
+				},
+				"outputType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"paramOptionValue": {
+					"type": "literal",
+					"value": "2"
+				},
+				"parameterName": {
+					"type": "literal",
+					"value": "a"
+				},
+				"parameterDefault": {
+					"type": "literal",
+					"value": "0"
+				},
+				"outputIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_result"
+				},
+				"stateMachine": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_StateMachine"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill"
+				},
+				"outputRequired": {
+					"type": "literal",
+					"value": "false"
+				},
+				"parameterIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_a"
+				},
+				"parameterRequired": {
+					"type": "literal",
+					"value": "true"
+				}
+			},
+			{
+				"parameterType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"outputIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_result"
+				},
+				"stateMachine": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_StateMachine"
+				},
+				"outputName": {
+					"type": "literal",
+					"value": "result"
+				},
+				"currentStateTypeIri": {
+					"type": "uri",
+					"value": "http://www.hsu-ifa.de/ontologies/ISA-TR88#Idle"
+				},
+				"skill": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill"
+				},
+				"outputRequired": {
+					"type": "literal",
+					"value": "false"
+				},
+				"parameterIri": {
+					"type": "uri",
+					"value": "https://siemens.de/skills#OpcUaSkill_b"
+				},
+				"outputType": {
+					"type": "literal",
+					"value": "Integer"
+				},
+				"parameterName": {
+					"type": "literal",
+					"value": "b"
+				},
+				"parameterRequired": {
+					"type": "literal",
+					"value": "true"
+				},
+				"parameterDefault": {
+					"type": "literal",
+					"value": "0"
+				}
+			}
+		]
+	}
+};
